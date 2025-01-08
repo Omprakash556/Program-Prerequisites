fe51cac0214164135f4684d1422cdef60cad9c2d
Om Prakash <ompandit416@gmail.com>
2025-01-08 08:24:47 +0530

    Add program of Calculate simple interest

---
+++ CalculateSimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalculateSimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Principle :");
+        int principle = sc.nextInt();
+        System.out.println("Enter Rate :");
+        int rate = sc.nextInt();
+        System.out.println("Enter Time :");
+        int time = sc.nextInt();
+        System.out.println("After Calculating Simple Interest is = "+(principle*time*rate)/100);
+    }
+}
